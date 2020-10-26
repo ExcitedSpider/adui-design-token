991c6bd8d40d835912f9e24a5e0f0cef18a155bc
qefeng <qefeng@tencent.com>
2020-10-26 14:56:19 +0800

    feat: 添加字体规范到变量中

--- src/var/index.ts
+++ src/var/index.ts
@@ -4,7 +4,7 @@ import font from './font';
 export default {
   ...palettes,
   // 暂时确定标准
-  // ...font,
+  ...font,
   // border: `1px solid var(--gray-100, ${palettes.gray100})`,
   // unit: '1px',
 };
