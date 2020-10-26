088cc5b0a8c6aae2baf092a05de81ff12e590355
qefeng <qefeng@tencent.com>
2020-10-26 14:43:23 +0800

    feat: 色板变量添加图表相关规范

--- src/var/index.ts
+++ src/var/index.ts
@@ -3,7 +3,8 @@ import font from './font';
 
 export default {
   ...palettes,
-  ...font,
-  border: `1px solid var(--gray-100, ${palettes.gray100})`,
-  unit: '1px',
+  // 暂时确定标准
+  // ...font,
+  // border: `1px solid var(--gray-100, ${palettes.gray100})`,
+  // unit: '1px',
 };
