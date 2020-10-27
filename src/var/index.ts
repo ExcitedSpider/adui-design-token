66af11323532da843c8fcb311d421e2ac3c4068d
qefeng <qefeng@tencent.com>
2020-10-27 15:48:27 +0800

    chore: 修改一些注释问题

--- src/var/index.ts
+++ src/var/index.ts
@@ -2,8 +2,9 @@ import palettes from './palettes';
 import font from './font';
 
 export default {
+  // 色板相关
   ...palettes,
-  // 暂时确定标准
+  // 字体相关
   ...font,
   // border: `1px solid var(--gray-100, ${palettes.gray100})`,
   // unit: '1px',
--- src/var/palettes.ts
+++ src/var/palettes.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 import tinycolor from 'tinycolor2';
 
 export default {
