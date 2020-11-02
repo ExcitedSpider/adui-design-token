f49687bb452a5cacef2a49a1cf176cb46cf0e52e
qefeng <qefeng@tencent.com>
2020-11-02 11:08:31 +0800

    feat: 添加一些注释

--- config/project-config.js
+++ config/project-config.js
@@ -1,7 +1,10 @@
 import { join } from 'path';
 
 export default {
+  /** web 和 mobile 公用的 token 变量 */
   externalCommonToken: [join(__dirname, '../external/common.json')],
+  /** web 专用的 token 变量 */
   externalWebToken: [],
+  /** mobile 专用的 token 变量 */
   externalMobileToken: [],
 };
