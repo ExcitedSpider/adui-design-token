a606e871e93980ba1abc22ff0c193762bfce42c5
qefeng <qefeng@tencent.com>
2020-11-02 10:51:23 +0800

    fix: 修复编译问题

--- build/token-compiler.js
+++ build/token-compiler.js
@@ -3,7 +3,7 @@ const kebabCase = require('lodash.kebabcase');
 module.exports = (opt = { kebabCase: true }) => (tokenObj) => {
   return {
     tokens: Object.keys(tokenObj).map((key) => ({
-      key: opt?.kebabCase ? kebabCase(key) : key,
+      key: opt.kebabCase ? kebabCase(key) : key,
       value: tokenObj[key],
     })),
   };
