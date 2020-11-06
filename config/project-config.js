961d334f4b9b3aae529f27e729992e9143aee664
qefeng <qefeng@tencent.com>
2020-11-06 19:46:41 +0800

    fix: 删除 Path 的引用

--- config/project-config.js
+++ config/project-config.js
@@ -1,8 +1,6 @@
-import { join } from 'path';
-
 export default {
   /** web 和 mobile 公用的 token 变量 */
-  externalCommonToken: [join(__dirname, '../external/common.json')],
+  externalCommonToken: ['../external/common.json'],
   /** web 专用的 token 变量 */
   externalWebToken: [],
   /** mobile 专用的 token 变量 */
