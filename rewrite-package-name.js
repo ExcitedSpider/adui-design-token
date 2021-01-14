52c549a2f2381a23fc8020e94db69ac041e95a0b
qefeng <qefeng@tencent.com>
2021-01-14 15:42:00 +0800

    feat: rename script

---
+++ rewrite-package-name.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const yargs = require('yargs');
+const { writeFileSync } = require('fs');
+
+function getParams() {
+  return yargs
+    .options({
+      input: {
+        alias: ['prefix', 'p'],
+        type: 'string',
+        desc: 'package prefix',
+        demandOption: true,
+      },
+    })
+    .help()
+    .version().argv;
+}
+
+function main() {
+  const { prefix } = getParams();
+  const pkg = require('./package.json');
+
+  pkg.name = `${prefix}/${pkg.name}`;
+  writeFileSync('./package.json', JSON.stringify(pkg, null, 2));
+}
+
+main();
